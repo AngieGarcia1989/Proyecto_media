fe073838a985ba7f47c14e0eeefffcd9c76a58c5
Angie Garcia <Angiegarciaoficial@gmail.com>
2020-08-08 01:35:28 -0500

    funciones

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,42 +1,22 @@
+// Funciones
+function add(a: number, b: number): number {
+    return a + b;
+  }
 
-// Boolean
-let muted: boolean = true;
-muted = false;
+  const sum = add(4, 6);
 
-
-  // Números
-let age = 6;
-let numerador: number = 42;
-let denominador: number = age;
-let resultado = numerador / denominador;
-
-// String
-let nombre: string = 'Richard';
-let saludo = `Me llamo ${nombre}`;
-
-// Arreglos
-let people: string[] = [];
-people = ['Isabel', 'Nicole', 'Raul'];
-// people.push("9000")
-
-let peopleAndNumbers: Array<string | number> = [];
-peopleAndNumbers.push('Ricardo');
-peopleAndNumbers.push(9001);
-
-// Enum
-enum Color {
-  Rojo = 'Rojo',
-  Verde = 'Verde',
-  Azul = 'Azul',
-  Amarillo = 'Amarillo',
+  function createAdder(a: number): (number) => number {
+    return function(b: number) {
+      return b + a;
+    };
 }
 
-let colorFavorito: Color = Color.Amarillo;
-console.log(`Mi color favorito es ${colorFavorito}`);
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
 
-// Any
-let comodin: any = 'Joker';
-comodin = { type: 'Wildcard' };
+function fullName(firstName: string, lastName: string = 'Smith'): string {
+    return `${firstName} ${lastName}`;
+  }
 
-// Object
-let someObject: object = { type: 'Wildcard' };
+  const richard = fullName('Agente');
+  console.log(richard);
